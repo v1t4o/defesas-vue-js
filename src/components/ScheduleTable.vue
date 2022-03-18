6a0a433ca95ac9ea7ba74719b302efaf5a563198
Victor de Oliveira <vitor.oliveira.marinho@gmail.com>
2022-03-18 16:43:19 -0300

    Ajusta layout e adiciona barra de pesquisa

--- src/components/ScheduleTable.vue
+++ src/components/ScheduleTable.vue
@@ -1,33 +1,18 @@
 <template>
   <v-row justify="center">
     <v-col sm="12" md="10">
-      <v-card dark>
+      <v-card color="grey lighten-4">
         <v-card-title>
           Agendamentos - Defesas da Pós Graduação
         </v-card-title>
         <v-card-text>
-          <v-simple-table>
-            <thead>
-              <tr>
-                <th>Data e Horário</th>
-                <th>Título</th>
-                <th>Candidato(a)</th>
-                <th>Nível</th>
-                <th>Programa</th>
-                <th>Orientador</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="schedule in schedules" :key="schedule.id">
-                <td>{{ schedule.data_horario }}</td>
-                <td>{{ schedule.titulo }}</td>
-                <td>{{ schedule.nome }}</td>
-                <td>{{ schedule.nivel }}</td>
-                <td>{{ schedule.programa }}</td>
-                <td>{{ schedule.orientador }}</td>
-              </tr>
-            </tbody>
-          </v-simple-table>
+          <v-data-table
+            :headers="headers"
+            :items="schedules"
+            :items-per-page="20"
+            class="elevation-1"
+          >
+          </v-data-table>
         </v-card-text>
       </v-card>
     </v-col>
@@ -38,7 +23,19 @@
 <script>
 
 export default{
-  props: ['schedules']
+  props: ['schedules'],
+  data () {
+    return {
+      headers: [
+        {text: 'Data e Horário', value: 'data_horario'}, 
+        {text: 'Título', value: 'titulo'}, 
+        {text: 'Candidato(a)', value: 'nome'}, 
+        {text: 'Nível', value: 'nivel'}, 
+        {text: 'Programa', value: 'programa'}, 
+        {text: 'Orientador', value: 'orientador'}
+      ]
+    }
+  },
 }
 
 </script>
--- src/views/ScheduleList.vue
+++ src/views/ScheduleList.vue
@@ -2,7 +2,42 @@
   <div>
     <v-row justify="center">
       <v-col sm="12" md="10">
-        <v-text-field label="Buscar agendamentos" v-model="term"></v-text-field>
+        <v-card
+          color="grey lighten-4"
+          flat
+          tile
+        >
+          <v-toolbar color="grey lighten-4" dense>
+            <v-app-bar-nav-icon>
+              <v-img
+                max-height="30"
+                max-width="30"
+                src="http://www.fflch.usp.br/sites/fflch.usp.br/files/2019-11/06_fflch_simbolo.jpg"
+              ></v-img>
+            </v-app-bar-nav-icon>        
+
+            <v-toolbar-title>FFLCH - Agendamentos - Pós-Graduação</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+              
+            <v-text-field 
+              label="Buscar agendamentos" 
+              v-model="term"
+              hide-details
+              single-line
+            ></v-text-field>
+
+            <v-btn icon>
+              <v-icon>mdi-magnify</v-icon>
+            </v-btn>
+
+          </v-toolbar>
+        </v-card>
+      </v-col>
+    </v-row>
+    
+    <v-row justify="center">
+      <v-col>
         <ScheduleTable :schedules="filterSchedules"/>
       </v-col>
     </v-row>
@@ -31,7 +66,6 @@ export default ({
   methods: {
     async getSchedules () {
       const response = await fetch('https://defesas.fflch.usp.br/api/agendamentos')
-      console.log(response);
       return response.json()
     }
   },
